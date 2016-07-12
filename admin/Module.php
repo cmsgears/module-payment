5fad3a40b91579f30ee07848145da0ab378e9d26
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2016-07-12 07:32:48 +0530

    Added admin module.

---
+++ admin/Module.php
@@ -0,0 +1,30 @@
+<?php
+namespace cmsgears\payment\admin;
+
+// Yii Imports
+use \Yii;
+
+// CMG Imports
+
+class Module extends \cmsgears\core\common\base\Module {
+
+    public $controllerNamespace = 'cmsgears\payment\admin\controllers';
+
+	public $config 				= [ ];
+
+    public function init() {
+
+        parent::init();
+
+        $this->setViewPath( '@cmsgears/module-payment/admin/views' );
+    }
+
+	public function getSidebarHtml() {
+
+		$path	= Yii::getAlias( "@cmsgears" ) . "/module-payment/admin/views/sidebar.php";
+
+		return $path;
+	}
+}
+
+?>
