b0703e11d23d1585ef69f169f969435cf1e9bea0
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2018-08-17 14:19:31 +0530

    Splitted base component and added factory component.

--- common/components/Payment.php
+++ common/components/Payment.php
@@ -9,12 +9,11 @@
 
 namespace cmsgears\payment\common\components;
 
-// Yii Imports
-use Yii;
-use yii\base\Component;
+// CMG Imports
+use cmsgears\core\common\base\Component;
 
 /**
- * Payment component register the services provided by Payment Module.
+ * Payment component initialises the Payment Module.
  *
  * @since 1.0.0
  */
@@ -30,59 +29,12 @@ class Payment extends Component {
 
 	// Constructor and Initialisation ------------------------------
 
-	/**
-	 * Initialize the services.
-	 */
-	public function init() {
-
-		parent::init();
-
-		// Register components and objects
-		$this->registerComponents();
-	}
-
 	// Instance methods --------------------------------------------
 
 	// Yii parent classes --------------------
 
 	// CMG parent classes --------------------
 
-	// Cms -----------------------------------
-
-	// Properties ----------------
-
-	// Components and Objects ----
-
-	/**
-	 * Register the services.
-	 */
-	public function registerComponents() {
-
-		// Register services
-		$this->registerResourceServices();
-
-		// Init services
-		$this->initResourceServices();
-	}
-
-	/**
-	 * Registers resource services.
-	 */
-	public function registerResourceServices() {
-
-		$factory = Yii::$app->factory->getContainer();
-
-		$factory->set( 'cmsgears\payment\common\services\interfaces\resources\ITransactionService', 'cmsgears\payment\common\services\resources\TransactionService' );
-	}
-
-	/**
-	 * Initialize resource services.
-	 */
-	public function initResourceServices() {
-
-		$factory = Yii::$app->factory->getContainer();
-
-		$factory->set( 'transactionService', 'cmsgears\payment\common\services\resources\TransactionService' );
-	}
+	// Payment -------------------------------
 
 }
